df9aa15052a1b10f0d64dbd38a3aeb1bf41fbb3a
Alex <diretoria@internegs.com.br>
2022-09-28 15:03:37 -0400

    inicio dos estudos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="ALEX TESTANDO O PODER DO VUEJS"/>
   </div>
 </template>
 
